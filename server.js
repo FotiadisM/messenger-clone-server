0f0d0efd3dc9a2fe12909678b60e76ff89c56b03
FotiadisM <fotiadis.michalis20@gmail.com>
2019-12-30 17:54:00 +0200

    Added /login, need to fix synchronization

--- server.js
+++ server.js
@@ -14,8 +14,63 @@ const PORT = 3001
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'messenger';
+const options = {
+  useUnifiedTopology: true
+  // server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  // replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};
+
 
 const saltRounds = 10;
+
+const checkEmail = (db, email, callback) => {
+  db.collection('login').find({email: email}).toArray((err, result) => {
+    if(err) { console.log(err) }
+    if(result.length === 0) {
+      callback(true)
+    }
+    else {
+      callback(false);
+    }
+  });
+}
+
+const validateSignIn = (db, { email, password, }, callback) => {
+  db.collection('login').find({email: email}).toArray((err, result) => {
+    if(err) { console.log(err) }
+    if(result.length === 0) {
+      callback(false);
+    }
+    else {
+      bcrypt.compare(password, result[0].hash, (err, valid) => {
+        if(err) { console.log(err) }
+        callback(valid);
+      });
+    }
+  });
+}
+
+app.post('/login', (req,res) => {
+  const { email } = req.body;
+
+  MongoClient.connect(url, options, (err, client) => {
+    if(err) { console.log(err) }
+    const db = client.db(dbName);
+
+    validateSignIn(db, req.body, (valid) => {
+      if(valid) {
+        db.collection('users').find({email: email}).toArray((err, result) => {
+          if(err) { console.log(err) }
+          res.status(200).json(result[0]);
+        });
+      }
+      else {
+        res.json(null);
+      }
+    })
+    // client.close();
+  });
+})
 
 app.post('/register', (req, res) => {
   const { name , email, password } = req.body;
@@ -23,19 +78,36 @@ app.post('/register', (req, res) => {
   bcrypt.hash(password, saltRounds, (err, hash) => {
     if(err) { console.log(err) }
 
-    const item = {
-      email: email,
-      hash: hash
-    }
-
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
       if(err) { console.log(err) }
       const db = client.db(dbName);
-      db.collection('login').insertOne(item, (err, result) => {
-        if(err) { console.log(err) }
-        client.close();
-        res.status(200).json('User Inserted');
-      });
+
+      checkEmail(db, email, (exist) => {
+
+        if(exist) {
+          db.collection('login').insertOne({
+            email: email,
+            hash: hash
+          }, (err, result) => { if(err) console.log(err) });
+
+          db.collection('users').insertOne({
+            name: name,
+            email: email,
+            status: true,
+            friends: [],
+            joined: Date()
+          }, (err, result) => { if(err) console.log(err) });
+
+          db.collection('users').find({email: email}).toArray((err, result) => {
+            if(err) { console.log(err) }
+            res.status(200).json(result[0]);
+          });
+        }
+        else {
+          res.json(null);
+        }
+        // client.close();
+      })
     });
   });
 })
